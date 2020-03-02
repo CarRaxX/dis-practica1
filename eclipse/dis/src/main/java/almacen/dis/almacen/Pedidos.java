8f2cc74f1b8bb7c24724d7b68acfee3d0cf02da9
willemsito <guille2vs@gmail.com>
2020-03-02 20:16:52 +0100

    Modificado el archivo Pedidos

--- eclipse/dis/src/main/java/almacen/dis/almacen/Pedidos.java
+++ eclipse/dis/src/main/java/almacen/dis/almacen/Pedidos.java
@@ -2,32 +2,48 @@ package almacen.dis.almacen;
 
 public class Pedidos {
 
-	public enum Nivel {
-		BASICO, MEDIO, FLUIDO, NATIVO
-	};
+	private String productos;
+	private int cantidad;
+	private String destinatario;
+	private String fechaDeEntregaEstimada;
 
-	private String nombre;
-	private Nivel nivel;
-
-	public Pedidos(String nombre, Nivel nivel) {
-		this.nombre = nombre;
-		this.nivel = nivel;
+	public Pedidos(String productos, int cantidad, String destinatario, String fechaDeEntregaEstimada) {
+		this.productos = productos;
+		this.cantidad = cantidad;
+		this.destinatario = destinatario;
+		this.fechaDeEntregaEstimada = fechaDeEntregaEstimada;
 	}
 
-	public String getNombre() {
-		return nombre;
+	public String getproductos() {
+		return productos;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setproductos(String productos) {
+		this.productos = productos;
 	}
 
-	public Nivel getNivel() {
-		return nivel;
+	public int getcantidad() {
+		return cantidad;
 	}
 
-	public void setnivel(Nivel nivel) {
-		this.nivel = nivel;
+	public void setcantidad(int cantidad) {
+		this.cantidad = cantidad;
 	}
 
+	public String getdestinatario() {
+		return destinatario;
+	}
+
+	public void setdestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
+	
+	public String getfechaDeEntregaEstimada() {
+		return fechaDeEntregaEstimada;
+	}
+
+	public void setfechaDeEntregaEstimada(String fechaDeEntregaEstimada) {
+		this.fechaDeEntregaEstimada = fechaDeEntregaEstimada;
+	}
+	
 }
