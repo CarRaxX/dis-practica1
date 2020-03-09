ecba632402abeba1c234884abbb9280d4996071f
CarRaxX <xxcavapexx@gmail.com>
2020-03-09 15:04:38 +0100

    Clase Dirección creada y completada

--- eclipse/dis/src/main/java/almacen/dis/almacen/direccionEntrega/Direccion.java
+++ eclipse/dis/src/main/java/almacen/dis/almacen/direccionEntrega/Direccion.java
@@ -1,5 +1,58 @@
 package almacen.dis.almacen.direccionEntrega;
 
 public class Direccion {
+	
+	private String calle;
+	private int numero;
+	private int codigoPostal;
+	private String población;
+	private String pais;
 
+	public Direccion(String calle, int numero, int codigoPostal, String población, String pais) {
+		this.calle = calle;
+		this.numero = numero;
+		this.codigoPostal = codigoPostal;
+		this.población = población;
+		this.pais = pais;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(int codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public String getPoblación() {
+		return población;
+	}
+
+	public void setPoblación(String población) {
+		this.población = población;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 }
